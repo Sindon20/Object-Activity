738d7c73b01a1d2529fb7a985711b499ebf2ebee
Sindon20 <ginessindonjhamaicamae@gmail.com>
2025-04-04 21:56:02 +0800

    Add files via upload

---
+++ objectActivity.js
@@ -0,0 +1,12 @@
+const car = {
+    type: "Sendan",
+    model: "Camry",
+    color: "Purple"
+};
+
+console.log(typeof car);
+car.type = "Toyota";
+console.log(car);
+
+car.wheels = 4;
+console.log(car);
